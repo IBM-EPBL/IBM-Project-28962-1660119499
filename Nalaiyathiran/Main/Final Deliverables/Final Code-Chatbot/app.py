57cef768f2ef90932b5345a3541fbeaa477d4a5b
Snehaamahesh <snehamahesh1809@gmail.com>
2022-11-17 14:57:52 +0530

    folder created

---
+++ Nalaiyathiran/Main/Final Deliverables/Final Code-Chatbot/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def bot():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.9', port=8080,debug=True)
